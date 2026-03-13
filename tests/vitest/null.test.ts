38112c69c7086e181f33b5c28e90066b580640c5
Darco2903 <william.valenduc@orange.fr>
2026-03-13 17:55:26 +0100

    Update CI, publish to npm

---
+++ tests/vitest/null.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+
+//////////////////////////
+// This is a placeholder test file to ensure that Vitest is set up correctly.
+
+describe("Placeholder test", () => {
+    it("should pass", () => {
+        expect(true).toBe(true);
+    });
+});
